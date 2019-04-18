41c016ea810d5810cc0aeec93d691791a8f3e00d
Ryan Karl <brightycloud@gmail.com>
2019-04-17 21:42:01 -0400

    Update data collection script to automatically create new DB when filename unspecified, and add more comments. Add the Makefile for arduino-mk to the git repository and update gitignore to add build directory.

--- charon/pyserial_timestamp.py
+++ charon/pyserial_timestamp.py
@@ -6,17 +6,27 @@ import sys
 import os
 import sqlite3
 
-if len(sys.argv) != 2:
+# TODO: proper command line options handling?
+
+if len(sys.argv) == 1:
+    # if database filename not specified, create default name with timestamp
+    db_name = time.strftime("%Y-%m-%d-%H%M%S_tempdata.db", time.gmtime())
+elif sys.argv[1] == '--help':
     print >> sys.stderr, "Usage: %s [logfile]" % sys.argv[0]
     sys.exit(1)
+else:
+    # if filename is specified, use that
+    db_name = sys.argv[1]
 
-if not os.path.isfile(sys.argv[1]):
-    conn = sqlite3.connect(sys.argv[1])
+if not os.path.isfile(db_name):
+    # if the database is new, open it and create the table for data
+    conn = sqlite3.connect(db_name)
     cur = conn.cursor()
-    cur.execute('''CREATE TABLE tempdata(timestamp integer, tempvalue real)''')
+    cur.execute('''CREATE TABLE tempdata (timestamp integer, tempvalue real)''')
     conn.commit()
 else:
-    conn = sqlite3.connect(sys.argv[1])
+    # otherwise, just connect
+    conn = sqlite3.connect(db_name)
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=5)
 
@@ -25,12 +35,16 @@ while ser.is_open:
     if line != '':
         timeval = time.time()
         timestamp = time.strftime("[%Y-%m-%dT%H:%M:%SZ]", time.gmtime(timeval))
+        # echo lines coming from the Arduino about Peltier switching, but don't log them (yet)
         if line.startswith("Peltier"):
+            # TODO: separate logging for switching of Peltier elements in database
             print("%s %s" % (timestamp, line[:-1]))
         else:
+            # convert the temperature value to a float and insert it into the database along with the timestamp
             tempval = float(line[:-1])
             cur = conn.cursor()
             cur.execute('''INSERT INTO tempdata VALUES (?, ?)''', (timeval, tempval))
             conn.commit()
-            print("%s %s" % (timestamp, line[:-1]))
+            # also echo the temperature data locally
+            print("%s Temperature: %s °C" % (timestamp, line[:-1]))
 
